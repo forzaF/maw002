f4a6ab01a15b22c60c67d9654cbbcd8749377f32
Andrew Lewis <Drew.lewis096@gmail.comm>
2020-03-21 17:41:16 -0400

    android setup

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { NavigationContainer} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button, View, Text } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Button, View, Text } from "react-native";
 
-import Home from './src/screens/home'
-import Profile from './src/screens/profile'
-import Wallet from './src/screens/wallet'
+import Home from "./src/screens/home";
+import Profile from "./src/screens/profile";
+import Wallet from "./src/screens/wallet";
 
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import Icon from 'react-native-vector-icons/Foundation'
-import { IconFill, IconOutline } from "@ant-design/icons-react-native"
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/Foundation";
+import { IconFill, IconOutline } from "@ant-design/icons-react-native";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,107 +18,104 @@ const CouldIt = createStackNavigator();
 
 function mawHome() {
   return (
-          <Tab.Navigator
-          screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            ioniconName = "md-home";
+            iconName = "home";
 
-              if (route.name === 'Home') {
-                  ioniconName = 'md-home'
-                  iconName = 'home'
-
-                  return  <Icon name={iconName} size={29} color={color} />
-              } else if (route.name === 'Wallet') {
-                  ioniconName = 'ios-card';
-                  iconName = 'credit-card';
-              } else if (route.name === 'Profile') {
-                  ioniconName = 'md-person';
-                  iconName = 'torso';
-          } else if (route.name === 'Feed') {
-            iconName = 'ios-paper-plane'
+            return <Icon name={iconName} size={29} color={color} />;
+          } else if (route.name === "Wallet") {
+            ioniconName = "ios-card";
+            iconName = "credit-card";
+          } else if (route.name === "Profile") {
+            ioniconName = "md-person";
+            iconName = "torso";
+          } else if (route.name === "Feed") {
+            iconName = "ios-paper-plane";
           }
-          
-              return <Ionicons name={ioniconName} size={29} color={color} />;
-              },
-          })}
-          tabBarOptions={{
-              activeTintColor: '#333',
-              inactiveTintColor: '#888',
-          },{
-            style: {
-              // position: "absolute",
-              borderTopColor: "transparent",
-              borderTopWidth: 0,
-              backgroundColor: '#fff',
-              // marginBottom: '6%',
-              // width: '70%',
-              // alignSelf: "center",
-              // marginLeft: '15%',
-              // marginRight: '15%',
-              // borderRadius: 30,
-              // height: 66,
-              // justifyContent: "center",
-              // paddingTop: '2%'
-            }, 
-            showLabel: false,
+
+          return <Ionicons name={ioniconName} size={29} color={color} />;
         }
-        
-        }
-          
-          >
-              <Tab.Screen name="Home" component={Home} />
-              <Tab.Screen name="Wallet" component={Wallet} />
-              {/* <Tab.Screen name="Profile" component={Profile} /> */}
-              </Tab.Navigator>
-      );
-  }
+      })}
+      tabBarOptions={
+        ({
+          activeTintColor: "#333",
+          inactiveTintColor: "#888"
+        },
+        {
+          style: {
+            // position: "absolute",
+            borderTopColor: "transparent",
+            borderTopWidth: 0,
+            backgroundColor: "#fff"
+            // marginBottom: '6%',
+            // width: '70%',
+            // alignSelf: "center",
+            // marginLeft: '15%',
+            // marginRight: '15%',
+            // borderRadius: 30,
+            // height: 66,
+            // justifyContent: "center",
+            // paddingTop: '2%'
+          },
+          showLabel: false
+        })
+      }
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Wallet" component={Wallet} />
+      {/* <Tab.Screen name="Profile" component={Profile} /> */}
+    </Tab.Navigator>
+  );
+}
 
-  function ModalScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-      </View>
-    );
-  }
+function ModalScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+    </View>
+  );
+}
 
-  function notificationsScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>Notifications!</Text>
-        <Button onPress={() => navigation.goBack()} title="Dismiss" />
-      </View>
-    );
-  }
+function notificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 30 }}>Notifications!</Text>
+      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    </View>
+  );
+}
 
-  function notifications () {
-    return(
-      <CouldIt.Navigator
-        screenOptions ={{
+function notifications() {
+  return (
+    <CouldIt.Navigator
+      screenOptions={{
         headerShown: false
+      }}
+    >
+      <CouldIt.Screen name="CINotifications" component={notificationsScreen} />
+    </CouldIt.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        mode="modal"
+        screenOptions={{
+          headerShown: false
         }}
       >
-        <CouldIt.Screen name="CINotifications" component={notificationsScreen} />
-      </CouldIt.Navigator>
-    )
-  }
-
-
-  function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                mode="modal"
-                screenOptions ={{
-                headerShown: false
-              }}
-            >
-              <Stack.Screen name="Home" component={mawHome} />
-              <Stack.Screen name="MyModal" component={ModalScreen} />
-              <Stack.Screen name="Notifications" component={notifications} />
-            </Stack.Navigator>
-        </NavigationContainer>
-        );
-    }
-
+        <Stack.Screen name="Home" component={mawHome} />
+        <Stack.Screen name="MyModal" component={ModalScreen} />
+        <Stack.Screen name="Notifications" component={notifications} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,148 +1,162 @@
 import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet,
-    TouchableOpacity,
-    TextInput,
-    Button,
-    TouchableWithoutFeedback,
-    Image,
-    SafeAreaView,
-    Alert
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Image,
+  SafeAreaView,
+  Alert
 } from "react-native";
-import {Header, Left, Right} from "native-base";
+import { Header, Left, Right } from "native-base";
 
 //Icons and gradient
-import Icon from 'react-native-vector-icons/MaterialIcons'
-import GradientIcon from '../components/custom-icons'
+import Icon from "react-native-vector-icons/MaterialIcons";
+import GradientIcon from "../components/custom-icons";
 
 //BottomSheet
-import BottomSheet from 'reanimated-bottom-sheet'
+import BottomSheet from "reanimated-bottom-sheet";
 
 // Components
-import Search from '../components/search'
-import Feed from '../components/feed'
+import Search from "../components/search";
+import Feed from "../components/feed";
 
 // Data
-import {activeJobs} from '../components/dev/data'
-
+import { activeJobs } from "../components/dev/data";
 
 let indicator;
 
-class Home extends Component   {
-    
-    constructor(props) {
-        super(props);
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            index: 0
-        }
-    }  
+    this.state = {
+      index: 0
+    };
+  }
 
-    componentDidMount() {
-        // this.setState = {
-        //     listOfJobs: activeJobs[0]
-        // }
+  componentDidMount() {
+    // this.setState = {
+    //     listOfJobs: activeJobs[0]
+    // }
 
-        indicator = 'component is mounting fine'
+    indicator = "component is mounting fine";
+  }
+
+  openModal = () => {
+    this.props.navigation.navigate("MyModal");
+  };
+
+  openNotifications = () => {
+    this.props.navigation.navigate("Notifications");
+  };
+
+  renderContent = () => {
+    let text;
+    if (this.state.index === 0) {
+      text = "NO ACTIVE JOBS YET";
+    } else if (this.state.index === 1) {
+      text = "NO ACTIVE JOBS YETTT";
     }
-
-    openModal = () => {
-        this.props.navigation.navigate('MyModal')
+    return (
+      <View style={{ backgroundColor: "white", height: "100%" }}>
+        <Feed
+          index={this.state.index}
+          indicator={indicator}
+          jobData={activeJobs}
+        />
+      </View>
+    );
+  };
+  //* Bottom Sheet Header Control Icon
+  renderHeaderControl = () => {
+    if (this.state.index === 0) {
+      return (
+        <View
+          style={{
+            width: 55,
+            height: 5,
+            borderRadius: 50 / 3,
+            backgroundColor: "#BDBEC8"
+          }}
+        ></View>
+      );
+    } else if (this.state.index === 1) {
+      return <Icon name="keyboard-arrow-down" size={44} color={"#BDBEC8"} />;
     }
+  };
 
-    openNotifications = () => {
-        this.props.navigation.navigate('Notifications')
+  renderHeader = () => (
+    <View
+      style={{
+        backgroundColor: "white",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingTop: 10
+      }}
+    >
+      {/* Bottom Sheet Header Control Icon */}
+      <View style={{ flex: 1, justifyContent: "center", alignSelf: "center" }}>
+        {this.renderHeaderControl()}
+      </View>
+    </View>
+  );
+
+  toggleIndex = () => {
+    if (this.state.index === 0) {
+      this.setState({ index: 1 });
+    } else if (this.state.index === 1) {
+      this.setState({ index: 0 });
     }
+  };
 
-    renderContent = () => {
-        let text ;
-        if(this.state.index === 0) {
-            text = "NO ACTIVE JOBS YET"
-        } else if(this.state.index === 1){
-            text = "NO ACTIVE JOBS YETTT"
-        }
-        return (
-            <View style={{backgroundColor: 'white', height: '100%'}}>
-                <Feed index={this.state.index} indicator={indicator} jobData={activeJobs} />
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header
+          style={[{ borderBottomWidth: 0 }, { backgroundColor: "#F2F2F9" }]}
+        >
+          {/* Feed button */}
+          <Left>
+            <TouchableOpacity onPress={this.openModal}>
+              <GradientIcon></GradientIcon>
+            </TouchableOpacity>
+          </Left>
+          <Right style={{ paddingRight: 18 }}>
+            <Icon
+              name="chat-bubble-outline"
+              size={25}
+              color={"rgba(0,0,0,.65)"}
+              style={{
+                transform: [{ scaleX: -1 }],
+                alignSelf: "flex-start",
+                marginTop: 17
+              }}
+              onPress={this.openNotifications}
+            ></Icon>
+            <View style={{ marginLeft: "10%", paddingTop: "5%" }}>
+              <Image
+                source={require("../dev_assests/img/primary_user.jpg")}
+                style={{ height: 38, width: 38, borderRadius: 38 / 2 }}
+              />
             </View>
-        )
-    }
-    //* Bottom Sheet Header Control Icon
-    renderHeaderControl = () => {
-        if(this.state.index === 0) {
-           return(
-            <View style={{width: 55, height: 5, borderRadius: 50/3, backgroundColor: '#BDBEC8'}}></View>
-           )
-        } else if (this.state.index === 1) {
-            return(
-                <Icon name='keyboard-arrow-down' size={44} color={'#BDBEC8'}/>
-            )
-        }
-    }
-    
-      renderHeader = () => (
-          
-        <View style={{backgroundColor: 'white', borderTopLeftRadius: 30, borderTopRightRadius: 30, paddingTop: 10}}>
-            {/* Bottom Sheet Header Control Icon */}
-        <View style={{flex: 1, justifyContent: "center", alignSelf: 'center'}}>
-            {this.renderHeaderControl()}
-        </View>
-        </View>
-      )
-
-      toggleIndex = () => {
-          if(this.state.index === 0) {
-            this.setState({ index: 1})
-          } else if (this.state.index === 1) {
-            this.setState({ index: 0})
-          }
-        
-      }
-          
-
-    render() {
-        return (
-            <View style={{flex: 1}}>
-                <Header style={[{borderBottomWidth: 0}, {backgroundColor: '#F2F2F9'}]}>
-                    {/* Feed button */}
-                    <Left><TouchableOpacity
-                        onPress={this.openModal}
-                    >
-                        <GradientIcon></GradientIcon>
-                        </TouchableOpacity>
-                    </Left>
-                    <Right style={{paddingRight: 18}}>
-                        <Icon 
-                        name="chat-bubble-outline" 
-                        size={25}
-                        color={'rgba(0,0,0,.65)'}
-                        style={{ transform: [{ scaleX: -1 }], alignSelf: "flex-start", marginTop: 17}}
-                        onPress={this.openNotifications}
-                        ></Icon>
-                        <View style={{marginLeft: '10%', paddingTop: '5%'}}>
-                            <Image                       
-                                source={require('../dev_assests/img/primary_user.jpg')}
-                                style={{height: 38, width: 38, borderRadius: 38/2, }}/>
-                        </View>
-                    </Right> 
-                </Header>
-                <Search/>
-                <BottomSheet
-                    snapPoints = {['10%','40%','80%']}
-                    renderContent = {this.renderContent}
-                    renderHeader = {this.renderHeader}
-                    onOpenStart={this.toggleIndex}
-                    onCloseEnd={this.toggleIndex}
-                    enabledContentGestureInteraction={true}
-                />
-
-
-            </View>
-        );
-}
+          </Right>
+        </Header>
+        <Search />
+        <BottomSheet
+          snapPoints={["10%", "40%", "80%"]}
+          renderContent={this.renderContent}
+          renderHeader={this.renderHeader}
+          onOpenStart={this.toggleIndex}
+          onCloseEnd={this.toggleIndex}
+          enabledContentGestureInteraction={true}
+        />
+      </View>
+    );
+  }
 }
 
 export default Home;
-
